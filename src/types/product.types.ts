16a288521673eac2377c97dd77f832dd65bbe8a2
Sofia Libertad <soliber.dev@gmail.com>
2025-07-08 21:28:09 -0500

    Add: Types for data in json files

---
+++ src/types/product.types.ts
@@ -0,0 +1,82 @@
+export type ProductCategoryId =
+  | 'figures'
+  | 'clothing'
+  | 'decor'
+  | 'drinks'
+  | 'snacks'
+  | 'accessories';
+
+export interface Translations {
+  es: string;
+  en: string;
+}
+
+export interface Product {
+  id: number;
+  slug: string;
+  name: Translations;
+  description: Translations;
+  currency: string;
+  price: number;
+  imageUrl: string;
+  categoryId: ProductCategoryId;
+  stock: number;
+  available: boolean;
+}
+
+export interface ProductCombo {
+  id: number;
+  name: Translations;
+  description: Translations;
+  products: number[];
+  currency: string;
+  comboPrice: number;
+}
+
+export interface ProductCategory {
+  id: ProductCategoryId;
+  name: Translations;
+  description: Translations;
+  icon: string;
+}
+
+export interface DiscountCondition {
+  category: ProductCategoryId;
+  minQuantity: number;
+  discount: string;
+}
+
+export interface ShippingCondition {
+  minPurchase: number;
+}
+
+export interface GiftCondition {
+  comboPurchase: boolean;
+  gift: string;
+}
+
+export interface DiscountPromotion {
+  id: string;
+  type: 'discount';
+  description: Translations;
+  condition: DiscountCondition;
+}
+
+export interface ShippingPromotion {
+  id: string;
+  type: 'shipping';
+  description: Translations;
+  condition: ShippingCondition;
+}
+
+export interface GiftPromotion {
+  id: string;
+  type: 'gift';
+  description: Translations;
+  condition: GiftCondition;
+}
+
+export type ProductPromotion = 
+  | DiscountPromotion
+  | ShippingPromotion
+  | GiftPromotion;
