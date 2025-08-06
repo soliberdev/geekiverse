035e1b33ba40a7f2ed5b2aaa95110323454e567c
Sofia Libertad <soliber.dev@gmail.com>
2025-08-05 22:49:40 -0500

    Update: Global styles and noise overlay to increase Z index

--- src/app/components/ui/NoiseOverlay.tsx
+++ src/app/components/ui/NoiseOverlay.tsx
@@ -1,7 +1,7 @@
 export default function NoiseOverlay() {
   return (
     <div
-        className="fixed inset-0 w-screen h-screen z-10 pointer-events-none opacity-5 bg-[url(/img/noise.gif)]"
+        className="fixed inset-0 w-screen h-screen z-50 pointer-events-none opacity-5 bg-[url(/img/noise.gif)]"
         aria-hidden="true"
     ></div>
   )
